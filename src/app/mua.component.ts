08bdbe12ad37e18a6eff27205347fdfa366ce3b6
VladislavOoll <apahiphip@mail.ru>
2021-10-24 23:42:08 +0300

    featch: Angular Services

--- src/app/mua.component.ts
+++ src/app/mua.component.ts
@@ -1,30 +1,18 @@
 import {Component} from '@angular/core';
-import {Subscription, Subject} from 'rxjs';
+import {MuaCounterService} from './services/mua-counter.service';
+import {LocalCounterService} from './services/local-counter.service';
 
 @Component({
   selector: 'mua-root',
   templateUrl: './mua.component.html',
   styleUrls: ['./mua.component.scss'],
+  providers: [LocalCounterService]
 })
 export class MuaComponent {
 
-  sub: Subscription;
-
-  stream$: Subject<number> = new Subject<number>();
-  counter = 0;
-
-  constructor() {
-    this.sub = this.stream$.subscribe(value => {
-      console.log('subscribe ', value);
-    });
-  }
-
-  stop() {
-    this.sub.unsubscribe();
-  }
-
-  next() {
-    this.stream$.next(++this.counter);
-  }
+  constructor(
+    public muaCounterService: MuaCounterService,
+    public localCounterService: LocalCounterService
+  ) {}
 
 }
--- src/app/mua.module.ts
+++ src/app/mua.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { MuaComponent } from './mua.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     MuaComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [MuaComponent]
 })
 export class MuaModule { }
