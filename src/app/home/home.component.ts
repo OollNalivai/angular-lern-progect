be875d95c14c160b8be26197eea25900cdcd5e29
VladislavOoll <apahiphip@mail.ru>
2021-11-19 19:29:05 +0300

    featch: routerLinkActive, routerLinkActiveOptions, router navigate. Fix list posts.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'mua-home',
@@ -7,4 +8,10 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private router: Router) {
+  }
+
+  goToPostPages() {
+    this.router.navigate(['/posts']).then(r => this.router);
+  }
 }
