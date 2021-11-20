d06430cfc3575bb2d333bf180db9fa088d17fab8
VladislavOoll <apahiphip@mail.ru>
2021-11-20 18:11:19 +0300

    featch: dynamic posts and switch to post 4

--- src/app/mua-routing.module.ts
+++ src/app/mua-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {PostsComponent} from './posts/posts.component';
 import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
+import {PostComponent} from './post/post.component';
 
 // http://localhost:4200/ => home
 // http://localhost:4200/about => about
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'about', component: AboutComponent },
   {path: 'posts', component: PostsComponent },
+  {path: 'posts/:id', component: PostComponent}
 ];
 
 @NgModule({
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,9 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Post, PostsService} from '../posts.service';
 
 @Component({
   selector: 'mua-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  post?: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private postsService: PostsService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params);
+      this.post = this.postsService.getById(+params.id);
+    });
+  }
+
+  LoadPost() {
+    this.router.navigate(['/posts', 44]);
+  }
 }
