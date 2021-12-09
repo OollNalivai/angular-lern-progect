5197cb8ed3cb297573ec949a6bc1eb9001bb276a
VladislavOoll <apahiphip@mail.ru>
2021-12-10 00:27:27 +0300

    init: init angular animation

--- src/app/mua.component.ts
+++ src/app/mua.component.ts
@@ -1,7 +1,4 @@
-import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
-import {ModalComponent} from './modal/modal.component';
-import {RefDirective} from './ref.directive';
-import {Meta, Title} from '@angular/platform-browser';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'mua-root',
@@ -11,37 +8,6 @@ import {Meta, Title} from '@angular/platform-browser';
 
 export class MuaComponent {
 
-  @ViewChild(RefDirective) refDir: RefDirective | undefined;
-
-  constructor(
-    private resolver: ComponentFactoryResolver,
-    private title: Title,
-    private meta: Meta
-  ) {
-    this.title.setTitle(`Oh shit i'm sorry`);
-    const t = title.getTitle();
-    console.log(t);
-    this.meta.addTags([
-      {name: 'keywords', content: 'angular, react'},
-      {name: 'description', content: 'this is Mua component'}
-    ]);
-  }
-
-  showModal() {
-    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
-
-    this.refDir?.containerRef.clear();
-
-    const component = this.refDir?.containerRef.createComponent(modalFactory);
-
-    if (component) {
-      component.instance.title = 'Dynamic title';
-    }
-    component?.instance.close.subscribe(() => {
-      this.refDir?.containerRef.clear();
-    });
-
-  }
 }
 
 
--- src/app/mua.module.ts
+++ src/app/mua.module.ts
@@ -3,24 +3,17 @@ import {NgModule} from '@angular/core';
 
 import {MuaComponent} from './mua.component';
 import {FormsModule} from '@angular/forms';
-import {ModalComponent} from './modal/modal.component';
-import {RefDirective} from './ref.directive';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     MuaComponent,
-    ModalComponent,
-    RefDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
   ],
-  providers: [],
-  entryComponents: [ModalComponent],
   bootstrap: [MuaComponent],
 })
 export class MuaModule {
