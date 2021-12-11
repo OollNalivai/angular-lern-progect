a513240b8175cee3dfa74cce4239960ea8461823
VladislavOoll <apahiphip@mail.ru>
2021-12-11 17:22:04 +0300

    eatch: static animations

--- src/app/mua.component.ts
+++ src/app/mua.component.ts
@@ -1,12 +1,25 @@
 import {Component} from '@angular/core';
+import {state, style, trigger} from '@angular/animations';
 
 @Component({
   selector: 'mua-root',
   templateUrl: './mua.component.html',
   styleUrls: ['./mua.component.scss'],
+  animations: [
+    trigger('boxTrig', [
+      state('start', style({
+        background: 'blue'
+      })),
+      state('end', style({
+        background: 'blue',
+        transform: 'scale{1.5}'
+      }))
+    ])
+  ]
 })
 
 export class MuaComponent {
+  boxState = 'start';
 
 }
 
