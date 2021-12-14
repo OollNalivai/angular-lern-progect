7d2ed2dc584ad5ddf34d566fc904cac0adee2750
VladislavOoll <apahiphip@mail.ru>
2021-12-14 23:41:56 +0300

    refactoring: animate

--- src/app/mua.animation.ts
+++ src/app/mua.animation.ts
@@ -1,55 +1,33 @@
 import {animate, group, keyframes, query, state, style, transition, trigger} from '@angular/animations';
 
-export const boxAnimation = trigger('boxTrig', [
+export const boxAnimation = trigger('myAnimationTrigger', [
   state('start', style({
-    backgroundColor: 'blue',
+    backgroundColor: 'black'
   })),
   state('end', style({
     backgroundColor: 'red',
-    transform: 'scale(1.2)',
+    fontSize: '24px'
   })),
-  state('special', style({
-    backgroundColor: 'black',
-    transform: 'scale(0.8)',
-    borderRadius: '50%',
-  })),
-  transition('start => end', animate(400)),
-  transition('end => start', animate('800ms ease-in-out')),
-  transition('special <=> *', [
-    group([
-      query('h4', animate(1500,
-        style({
-          fontSize: '2.5rem',
-          color: 'red',
-        }))),
-      style({background: 'green'}),
-      animate('0.5s',
-        style({
-          backgroundColor: 'pink',
-        }),
-      ),
-      animate(950),
-    ]),
-  ]),
-  // void => *
+  transition('start => end', animate(500)),
+  transition('end => start', animate(500)),
   transition(':enter', [
     animate('4s', keyframes([
-      style({backgroundColor: 'red', offset: 0}),
-      style({backgroundColor: 'orange', offset: 0.2}),
-      style({backgroundColor: 'black', offset: 0.3}),
-      style({backgroundColor: 'blue', offset: 1}),
+      style({backgroundColor: 'red', marginLeft: '700px', offset: 0}),
+      style({backgroundColor: 'orange', marginLeft: '300px', offset: 0.3}),
+      style({backgroundColor: 'black', marginLeft: '200px', offset: 0.5}),
+      style({backgroundColor: 'blue', marginLeft: 0, offset: 1}),
     ])),
   ]),
-  // * => void
   transition(':leave', [
     style({opacity: 1}),
     group([
-      animate(850, style({
-        opacity: 0,
-        transform: 'scale(1.4)',
-      })),
-      animate(460, style({
-        color: '#000',
+      animate('4s', keyframes([
+        style({backgroundColor: 'red', marginLeft: 0, offset: 0}),
+        style({backgroundColor: 'orange', marginLeft: '200px', offset: 0.3}),
+        style({backgroundColor: 'black', marginLeft: '300px', offset: 0.5}),
+        style({backgroundColor: 'blue', marginLeft: '700px', offset: 1}),
+      ])),
+      animate(800, style({
         fontSize: '28px',
       })),
     ]),
--- src/app/mua.component.ts
+++ src/app/mua.component.ts
@@ -9,24 +9,16 @@ import {boxAnimation} from './mua.animation';
 })
 
 export class MuaComponent {
-  boxState = 'start';
+  myStatusExp = 'start';
   log = '';
   visible = true;
 
   boxStart() {
-    this.boxState = this.boxState === 'end' ? 'start' : 'end';
+    this.myStatusExp = this.myStatusExp === 'end' ? 'start' : 'end';
   }
 
   boxEnd() {
-    this.boxState = this.boxState === 'start' ? 'end' : 'start';
-  }
-
-  animationStart(event: any) {
-    console.log('animationStart', event.phaseName);
-  }
-
-  animationDone(event: any) {
-    console.log('animationDone', event.phaseName);
+    this.myStatusExp = this.myStatusExp === 'start' ? 'end' : 'start';
   }
 }
 
