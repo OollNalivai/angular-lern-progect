3234369f08f9e70b4d73feb53a26765d20e39c08
VladislavOoll <apahiphip@mail.ru>
2021-12-19 21:19:10 +0300

    create: post component. Added post to the page

--- src/app/mua.module.ts
+++ src/app/mua.module.ts
@@ -1,24 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { MuaRoutingModule } from './mua-routing.module';
-import { MuaComponent } from './mua.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PostPageComponent } from './post-page/post-page.component';
+import {MuaRoutingModule} from './mua-routing.module';
+import {MuaComponent} from './mua.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';
+import {PostComponent} from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
     MuaComponent,
     MainLayoutComponent,
     HomePageComponent,
-    PostPageComponent
+    PostPageComponent,
+    PostComponent,
   ],
-    imports: [
-        BrowserModule,
-        MuaRoutingModule,
-    ],
+  imports: [
+    BrowserModule,
+    MuaRoutingModule,
+  ],
   providers: [],
-  bootstrap: [MuaComponent]
+  bootstrap: [MuaComponent],
 })
-export class MuaModule { }
+export class MuaModule {
+}
