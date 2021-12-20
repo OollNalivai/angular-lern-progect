1db264ad4bba2c2faae2ed8d4dbcdc3eb84c00d1
VladislavOoll <apahiphip@mail.ru>
2021-12-21 00:38:43 +0300

    featch: interfaces, errors message logic

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {IUser} from '../../shared/interfaces';
 
 @Component({
   selector: 'mua-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup | undefined;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
-        Validators.email
+        Validators.email,
       ]),
       password: new FormControl(null, [
         Validators.required,
-        Validators.minLength(6)
-      ])
+        Validators.minLength(6),
+      ]),
     });
   }
 
   submit() {
+    console.log(this.form);
     if (this.form?.invalid) {
       return;
     }
+
+    const user: IUser = {
+      email: this.form?.value.email,
+      password: this.form?.value.password,
+    };
+
+    console.log(user);
+
   }
 }
