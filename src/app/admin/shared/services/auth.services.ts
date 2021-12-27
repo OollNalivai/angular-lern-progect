f60c6ebad881919f8b8142050da1b60086bc928c
VladislavOoll <apahiphip@mail.ru>
2021-12-28 00:08:33 +0300

    fix: types and change static setToken

--- src/app/admin/shared/services/auth.services.ts
+++ src/app/admin/shared/services/auth.services.ts
@@ -7,48 +7,51 @@ import {environment} from '../../../../environments/environment';
 import {tap} from 'rxjs/operators';
 
 @Injectable()
-
 export class AuthServices {
 
   constructor(private http: HttpClient) {
   }
 
   get token(): string | null {
-    const expData = new Date(localStorage.getItem('fb-token-exp') as string);
+    const expData = new Date(localStorage.getItem('fb-token-exp') || '');
+
     if (new Date() > expData) {
       this.logout();
+
       return null;
     }
+
     return localStorage.getItem('fb-token');
   }
 
-  login(user: IUser): Observable<any> {
-    user.returnSecureToken = true;
-    return this.http
-      .post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
-      .pipe(
-        tap(this.setToken) as any,
-      );
+  private static setToken(response: FbAuthResponse | null): void {
 
-  }
-
-  logout() {
-    this.setToken(null);
-  }
-
-  isAuthenticated(): boolean {
-    return !!this.token;
-  }
-
-  private setToken(response: FbAuthResponse | null): void {
     if (response) {
       console.log(response);
       const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+
       localStorage.setItem('fb-token', response.idToken);
       localStorage.setItem('fb-token-exp', expDate.toString());
     } else {
       localStorage.clear();
     }
+  }
 
+  login(user: IUser): Observable<FbAuthResponse | null> {
+    user.returnSecureToken = true;
+
+    return this.http
+      .post<FbAuthResponse | null>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(AuthServices.setToken),
+      );
+  }
+
+  logout() {
+    AuthServices.setToken(null);
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token;
   }
 }
