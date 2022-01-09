5f732087ff3b11879845f5b24e59c318ff8df592
VladislavOoll <apahiphip@mail.ru>
2022-01-09 23:15:32 +0300

    added: quill, update rxjs.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,19 @@
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {QuillModule} from 'ngx-quill';
+import QuillType, { Delta } from 'quill';
+
+
 
 @NgModule({
-  imports: [HttpClientModule],
-  exports: [HttpClientModule]
+  imports: [
+    HttpClientModule,
+    QuillModule.forRoot()
+  ],
+  exports: [
+    HttpClientModule,
+    QuillModule
+  ]
 })
 
 export class SharedModule {
