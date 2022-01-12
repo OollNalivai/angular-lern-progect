53142f910ba28c7228689b0db0279807ec928eac
VladislavOoll <apahiphip@mail.ru>
2022-01-13 00:33:27 +0300

    featch: added create post, url - fbDbUrl. fix interface

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IPost } from './interfaces';
+import { environment } from '../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 
 export class PostsService {
 
   constructor(private http: HttpClient) {
+  }
+
+  create(post: IPost): Observable<IPost> {
+    return this.http.post<IPost>(`${environment.fbDbUrl}/posts.json`, post);
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: `AIzaSyADcuxva7-GC2KY7eQfkp7YjCjvupeZHDk`
+  apiKey: `AIzaSyADcuxva7-GC2KY7eQfkp7YjCjvupeZHDk`,
+  fbDbUrl: `https://mua-angular-project-default-rtdb.europe-west1.firebasedatabase.app/`
 };
 
 /*
