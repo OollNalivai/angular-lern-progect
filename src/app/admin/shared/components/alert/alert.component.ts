3ea07d5f74ac594bffdf13f890501b513154036f
VladislavOoll <apahiphip@mail.ru>
2022-01-31 23:15:36 +0300

    featch: adding warnings with a delay

--- src/app/admin/shared/components/alert/alert.component.ts
+++ src/app/admin/shared/components/alert/alert.component.ts
@@ -1,20 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { AlertService } from '../../services/alert.service';
+import { Subscription, timeout } from 'rxjs';
 
 @Component({
   selector: 'mua-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent implements OnInit, OnDestroy {
 
   @Input() delay = 5000;
 
   public text: string | undefined;
   public type = 'success';
+  _subscriptions$: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private alertService: AlertService) {
+  }
 
   ngOnInit(): void {
+    this._subscriptions$.add(
+      this.alertService.alert$.subscribe(alert => {
+        this.text = alert.text;
+        this.type = alert.type;
+
+        const timeout = setTimeout(() => {
+          clearTimeout(timeout);
+          this.text = '';
+        }, this.delay);
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions$.unsubscribe();
   }
 
 }
