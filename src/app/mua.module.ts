49bf0e1704f02a393f2ce36d5c19d9f618c328d0
VladislavOoll <apahiphip@mail.ru>
2022-02-10 23:16:31 +0300

    fix: structural fix

--- src/app/mua.module.ts
+++ src/app/mua.module.ts
@@ -6,28 +6,20 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MuaRoutingModule } from './mua-routing.module';
 import { MuaComponent } from './mua.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { PostComponent } from './shared/components/post/post.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthInterceptor } from './shared/auth.interceptor';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SliderComponent } from './slider/slider.component';
 
 registerLocaleData(enLocaleData, 'en')
 
 @NgModule({
   declarations: [
     MuaComponent,
-    MainLayoutComponent,
     HomePageComponent,
     PostPageComponent,
     PostComponent,
-    FooterComponent,
-    HeaderComponent,
-    SliderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { QuillModule } from 'ngx-quill';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
+    RouterModule,
     HttpClientModule,
     QuillModule.forRoot()
   ],
@@ -12,11 +18,13 @@ import { QuillModule } from 'ngx-quill';
     QuillModule
   ],
   declarations: [
+    FooterComponent,
+    HeaderComponent,
+    SliderComponent,
+    MainLayoutComponent,
   ]
 })
 
 export class SharedModule {
 
-  constructor() {
-  }
 }
