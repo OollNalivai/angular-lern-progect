6a45dbc8deb60fc3caeeeefa2bfcaa7f90a9155f
VladislavOoll <apahiphip@mail.ru>
2022-02-16 00:27:50 +0300

    featch: fix fake array

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
 import { Post } from '../../interfaces';
-import { Observable } from 'rxjs';
+import { delay, Observable, skipWhile } from 'rxjs';
 import { PostsService } from '../../posts.service';
 
 @Component({
@@ -8,16 +8,14 @@ import { PostsService } from '../../posts.service';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit, AfterContentChecked {
 
-  @Input() post!: Post;
-
-  array = [1111111, 2222222, 3333333, 4444444, 5555555, 6666666, 7777777];
-  start: number = 0;
-  end: number = 3;
+  sliceStart: number = 0;
+  sliceEnd: number = 3;
   sliderItem = document.getElementsByClassName('slider__item');
   btnLeft = document.getElementsByClassName('left');
   btnRight = document.getElementsByClassName('right');
+  postsArr: any[] = [];
 
   public posts$: Observable<Post[]> | undefined;
 
@@ -27,14 +25,24 @@ export class SliderComponent {
 
   ngOnInit(): void {
     this.posts$ = this.postsService.allPosts;
+    this.postsService.allPosts
+      .pipe(skipWhile((posts: Post[]) => !posts.length))
+      .subscribe(posts => {
+        console.log(posts, 'posts');
+        this.postsArr = [...posts];
+    })
+    console.log(this.postsArr, 'this.posts');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log(this.postsArr, 'this.posts');
   }
 
   clickLeft() {
-    this.array.slice(this.start--, this.end--);
+    this.postsArr.slice(this.sliceStart--, this.sliceEnd--);
   }
 
   clickRight() {
-    this.array.slice(this.start++, this.end++);
-    console.log(this.sliderItem)
+    this.postsArr.slice(this.sliceStart++, this.sliceEnd++);
   }
 }
