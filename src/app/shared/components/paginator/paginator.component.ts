0ecff22504c5a75e9ebd5fefd44c2450f21942f5
VladislavOoll <apahiphip@mail.ru>
2022-02-20 23:58:55 +0300

    featch: event click to page. refactoring code

--- src/app/shared/components/paginator/paginator.component.ts
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -9,17 +9,28 @@ export class PaginatorComponent implements OnInit{
 
 
   arr = [1,33,3,4,5,6,6,7,7,3,3,2,2,34,4,2,1,2,3,5,34,5,65,7,56,8,6,8,8,9,5,4,3,2,1];
-  allPageLength: number[] = [];
-
+  arrPageNumbers: number[] = [];
+  totalPage: number = Math.ceil(this.arr.length/5);
+  currentPage: number | undefined;
   sliceStart: number = 30;
   sliceEnd: number = 35;
 
+  currentEl:  Element | null = document.querySelector('.tools-paginator__button');
+
   ngOnInit(): void {
-    for (let i = 1; i <= Math.ceil(this.arr.length/5); i++ ) {
-      this.allPageLength.push(i);
+    for (let i = 1; i <= this.totalPage; i++ ) {
+      this.arrPageNumbers.push(i);
     }
     console.log(this.arr.length);
-    console.log(this.allPageLength);
+    console.log(this.arrPageNumbers);
   }
 
+  clickPage($event: any): void {
+    this.currentPage = $event.target.innerHTML.replace(/[^0-9]/g,"");
+    // this.currentEl?.classList.add('active');
+    this.sliceStart = this.currentPage! * 5 -5;
+    this.sliceEnd = this.currentPage! * 5;
+
+    console.log(this.currentPage);
+  }
 }
