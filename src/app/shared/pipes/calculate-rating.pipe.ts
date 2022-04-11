aef57daf096c4bb1bd03a5fa8b4a8cf367b519e0
VladislavOoll <apahiphip@mail.ru>
2022-04-11 23:47:59 +0300

    refactoring: Calculate Rating Pipe

--- src/app/shared/pipes/calculate-rating.pipe.ts
+++ src/app/shared/pipes/calculate-rating.pipe.ts
@@ -6,17 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class CalculateRatingPipe implements PipeTransform {
 
-  transform(scoreArray: number[] | undefined, calculatePercent?: boolean): number | string {
+  transform(scoreArray: number[] | undefined, calculate: 'average' | 'percentage'): number | string {
     let result: number | string = 0;
 
     if (scoreArray?.length) {
       result = scoreArray.reduce((acc, curr) => acc + curr) / scoreArray.length;
     }
 
-    if (calculatePercent) {
-      result = result ? result / 0.05 + '%' : result + '%'; // % от числа 5
+    switch (calculate) {
+      case 'average':
+        return result;
+      case 'percentage':
+        return result ? result / 0.05 + '%' : result + '%'; // % от числа 5
+      default:
+        return result;
     }
-
-    return result;
   }
 }
